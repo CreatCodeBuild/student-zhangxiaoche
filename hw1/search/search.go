01f0f1738dfa2ba3514bb9f6ae217b3d4bd49633
CreatCodeBuild <CreatCodeBuild@github.com>
2020-02-15 21:33:39 -0800

    reorg

---
+++ hw1/search/search.go
@@ -0,0 +1,41 @@
+package search
+
+import "fmt"
+import "io/ioutil"
+import "container/list"
+import "strings"
+
+func Search(logs, filter, separator string) (resultlist list.List) {
+
+	if logs == "" {
+		fmt.Println("未输入日志文件路径")
+		return
+	}
+	if filter == "" {
+		fmt.Println("未输入查找内容")
+		return
+	}
+	if separator == "" {
+		separator = "，"
+		fmt.Println("未输入分隔符，默认按，分割行")
+	}
+	//fmt.Println(logs,filter,separator)
+	fmt.Println("日志查找-结果：")
+	contents, err := ioutil.ReadFile(logs)
+	if err == nil {
+		//因为contents是[]byte类型，直接转换成string类型后会多一行空格,需要使用strings.Replace替换换行符
+		result := strings.Replace(string(contents), "\n", "", 1)
+		//按分割符分割字符串
+		section := strings.SplitAfter(result, separator)
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+		for i := 0; i < len(section); i++ {
+			if strings.Contains(section[i], filter) {
+				fmt.Println("-------------------------------------")
+				fmt.Println(section[i])
+				resultlist.PushBack(section[i])
+			}
+		}
+		fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+	}
+	return
+}
